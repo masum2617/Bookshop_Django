b7809f91f119f8e01320ae0a38880c046e393b5c
Masum Hasan <masum2617@gmail.com>
2021-08-21 00:13:17 +0600

    settings file changed for deploy

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q)v)2l9!3(4jqh-b_$r6i=bxc-bdmy^umf42p)@q*o4qkm4sa-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['brownbookshelfbd.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
