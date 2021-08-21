f131bfdf64035bed17204354e4bd221e1c2887bc
Masum Hasan <masum2617@gmail.com>
2021-08-21 13:06:11 +0600

    static settings changes

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bookshop/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
